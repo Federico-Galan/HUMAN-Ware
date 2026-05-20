9f63847dd7d0c025b72a0efdfbe5983317862273
Lucas Carreño <lucascarreno559@gmail.com>
2026-05-20 16:03:49 -0300

    OfertaLaboralDTO

--- src/dto/OfertaLaboralDTO.java
+++ src/dto/OfertaLaboralDTO.java
@@ -4,10 +4,50 @@
  */
 package dto;
 
+import enums.EstadoOferta;
+import enums.TipoJornada;
 /**
  *
  * @author 
  */
-public class OfertaLaboralDTO {
+public class OfertaLaboralDTO{
+    private int idOferta;
+    private String titulo;
+    private double salarioMin;
+    private double salarioMax;
+    private TipoJornada jornada;
+    private EstadoOferta estado;
     
+    public OfertaLaboralDTO(int idOferta, String titulo, double salarioMin, double salarioMax, TipoJornada jornada, EstadoOferta estado){
+        this.idOferta = idOferta;
+        this.titulo = titulo;
+        this.salarioMin = salarioMin;
+        this.salarioMax = salarioMax;
+        this.jornada = jornada;
+        this.estado = estado;
+    }
+    
+    public int getIdOferta(){
+        return idOferta;
+    }
+    
+    public String getTitulo(){
+        return titulo;
+    }
+    
+    public double getSalarioMin(){
+        return salarioMin;
+    }
+    
+    public double getSalarioMax(){
+        return salarioMax;
+    }
+    
+    public TipoJornada getJornada(){
+        return jornada;
+    }
+    
+    public EstadoOferta getEstado(){
+        return estado;
+    }
 }
